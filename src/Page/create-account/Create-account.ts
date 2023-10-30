5cd843575bdb6983e20e204c66a47d4923629453
SergeySabyrkin <sergeysabyrkin@gmail.com>
2023-10-30 21:35:28 +0300

    Add page create-account

---
+++ src/Page/create-account/Create-account.ts
@@ -0,0 +1,16 @@
+import s from "./Create-account.module.scss";
+import {FormCreateAccount} from "../../widgets/form-create-account/ui/Form-create-account";
+
+type CreateAccountType = {
+
+};
+
+export const CreateAccount = ({}: CreateAccountType) => {
+  return `
+    <section class="${s.createAccount}">
+        <div class="${s.body}">
+            ${FormCreateAccount({id: "form-create-account", onSubmit: () => {}})}
+        </div>
+    </section>
+  `
+};
