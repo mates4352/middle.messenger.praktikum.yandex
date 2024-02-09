a043818361e376f69d84cba61a68dbbf4e31033f
SergeySabyrkin <sergeysabyrkin@gmail.com>
2024-02-09 19:42:36 +0300

    Add type for InputText

--- src/components/atom/input-text/index.ts
+++ src/components/atom/input-text/index.ts
@@ -3,6 +3,13 @@ import tpl from "tpl.hbs?raw";
 
 Handlebars.registerPartial("InputText", tpl);
 
-export const InputText = () => {
-  return Handlebars.compile(tpl)({});
+type InputTextType = {
+  id: string;
+  value: string;
+  type: string;
+  name: string;
+};
+
+export const InputText = ({ id, value, type, name }: InputTextType) => {
+  return Handlebars.compile(tpl)({ id, value, type, name });
 };
