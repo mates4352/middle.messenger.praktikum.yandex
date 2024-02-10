af0b00e3a7a6e38ae915b389c189d6e3607ba341
SergeySabyrkin <sergeysabyrkin@gmail.com>
2024-02-10 11:48:14 +0300

    Add component profile-data-inputs

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -4,3 +4,4 @@ export * from "./button/index";
 export * from "./input-text/index";
 export * from "./button-text/index";
 export * from "./card-body/index";
+export * from "./profile-data-inputs/index";
--- src/components/pages/profile-data/index.ts
+++ src/components/pages/profile-data/index.ts
@@ -1,78 +1,80 @@
 import * as Handlebars from "handlebars";
 import tpl from "./index.hbs?raw";
-import { ButtonText, InputText } from "../../atom";
+import { ButtonText, InputText, ProfileDataInputs } from "../../atom";
 
 Handlebars.registerPartial("ProfileData", tpl);
 
 export const ProfileData = () => {
   return Handlebars.compile(tpl)({
-    profileDataContent: [
-      {
-        text: "Почта",
-        input: InputText({
-          className: "input-text-disabled",
-          id: "profile-data-input-text-email",
-          value: "pochta@yandex.ru",
-          type: "text",
-          name: "email",
-        }),
-      },
+    profileDataInputs: ProfileDataInputs({
+      inputs: [
+        {
+          text: "Почта",
+          input: InputText({
+            className: "input-text-disabled",
+            id: "profile-data-input-text-email",
+            value: "pochta@yandex.ru",
+            type: "text",
+            name: "email",
+          }),
+        },
 
-      {
-        text: "Логин",
-        input: InputText({
-          className: "input-text-disabled",
-          id: "profile-data-input-text-login",
-          value: "ivanivanov",
-          type: "text",
-          name: "login",
-        }),
-      },
+        {
+          text: "Логин",
+          input: InputText({
+            className: "input-text-disabled",
+            id: "profile-data-input-text-login",
+            value: "ivanivanov",
+            type: "text",
+            name: "login",
+          }),
+        },
 
-      {
-        text: "Имя",
-        input: InputText({
-          className: "input-text-disabled",
-          id: "profile-data-input-text-first-name",
-          value: "Иван",
-          type: "text",
-          name: "first_name",
-        }),
-      },
+        {
+          text: "Имя",
+          input: InputText({
+            className: "input-text-disabled",
+            id: "profile-data-input-text-first-name",
+            value: "Иван",
+            type: "text",
+            name: "first_name",
+          }),
+        },
 
-      {
-        text: "Фамилия",
-        input: InputText({
-          className: "input-text-disabled",
-          id: "profile-data-input-text-second-name",
-          value: "Иванов",
-          type: "text",
-          name: "second_name",
-        }),
-      },
+        {
+          text: "Фамилия",
+          input: InputText({
+            className: "input-text-disabled",
+            id: "profile-data-input-text-second-name",
+            value: "Иванов",
+            type: "text",
+            name: "second_name",
+          }),
+        },
 
-      {
-        text: "Имя в чате",
-        input: InputText({
-          className: "input-text-disabled",
-          id: "profile-data-input-text-display-name",
-          value: "Иван",
-          type: "text",
-          name: "display_name",
-        }),
-      },
+        {
+          text: "Имя в чате",
+          input: InputText({
+            className: "input-text-disabled",
+            id: "profile-data-input-text-display-name",
+            value: "Иван",
+            type: "text",
+            name: "display_name",
+          }),
+        },
 
-      {
-        text: "Телефон",
-        input: InputText({
-          className: "input-text-disabled",
-          id: "profile-data-input-text-phone",
-          value: "+7 (909) 967 30 30",
-          type: "text",
-          name: "phone",
-        }),
-      },
-    ],
+        {
+          text: "Телефон",
+          input: InputText({
+            className: "input-text-disabled",
+            id: "profile-data-input-text-phone",
+            value: "+7 (909) 967 30 30",
+            type: "text",
+            name: "phone",
+          }),
+        },
+      ],
+    }),
 
     profileDataList: [
       {
